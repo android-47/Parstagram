ffb3f3004c09219ffbacc9e6b0b9654f8e11203a
android-47 <jx.garcia@outlook.com>
2020-10-23 18:34:01 -0700

    User is able to sign up and login successfully if they enter the proper credentials.

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "8IPk27OYqviC43wXUzCHLLEn5C2s2ph7wlbErgB4" // <- UPDATE
+                $0.clientKey = "3pfamLeqoKMTZ05QS5h0Ruppb8bBaMI9PlJyjEsU" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
+        // --- end copy
+        
+        
+        
         return true
     }
 
